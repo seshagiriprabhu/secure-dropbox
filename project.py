2573a84711492e737036adae4e03c00b97d461c4
seshagiriprabhu <seshagiriprabhu@gmail.com>
2013-04-28 20:37:49 +0530

    Added encrypt and decrypt algo

--- project.py
+++ project.py
@@ -13,13 +13,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 # Include the python libraries
-import cmd
-import locale
-import os
-import pprint
-import shlex
-import sys
-import re
+import locale, cmd
+import os, random, struct
+import pprint, shlex, sys, re
+from Crypto.Cipher import AES
+from re import sub
 
 # Include the Dropbox SDK libraries
 from dropbox import client, rest, session
@@ -33,6 +31,8 @@ HELP = 'help'
 # You can find these values at http://www.dropbox.com/developers/apps
 APP_KEY = 'gy60gre2uauivp2'
 APP_SECRET = '3yvf1vsyj5y7pvo'
+KEY = "rU4Debxe2zKqSxNrUSUUk9AS"
+
 ACCESS_TYPE = 'dropbox' # should be 'dropbox' or 'app_folder' as configured for your app
 
 
@@ -41,9 +41,9 @@ class Dropbox(cmd.Cmd):
         cmd.Cmd.__init__(self)
 
         # If number of arguments is less than 4
-        if len(sys.argv) < 3:
+        if len(sys.argv) < 4:
             self.do_help()
-        #    sys.exit(0)
+            sys.exit(0)
 
         # If the TOKEN file exist
         if os.path.isfile(TOKENS):
@@ -73,13 +73,13 @@ class Dropbox(cmd.Cmd):
             token_file.close()
 
         print "Successfully authenticated!\nRetrieving user information from server\n"
-        api_client = client.DropboxClient(sess)
-        self.do_account_info(api_client)
+        self.api_client = client.DropboxClient(sess)
+        #self.do_account_info(api_client)
         
         if (sys.argv[1] == 'put'):
-            self.do_put(sys.argv[2])
+            self.do_put(sys.argv[2], sys.argv[3])
         elif (sys.argv[1] == 'get'):
-            self.do_get(sys.argv[2])
+            self.do_get(sys.argv[2], sys.argv[4])
         else:
             print "Invalid argument[1]"
 
@@ -89,7 +89,7 @@ class Dropbox(cmd.Cmd):
         pprint.PrettyPrinter(indent=2).pprint(f)
 
     # A function to download files from dropbox account
-    def do_get(self, from_path):
+    def do_get(self, from_path, to_path=None):
         """
         Copy file from Dropbox to local file and print out out the metadata.
 
@@ -105,7 +105,7 @@ class Dropbox(cmd.Cmd):
             sys.exit(0)
 
     # A function to upload files into dropbox account
-    def do_put(self, from_path):
+    def do_put(self, from_path, to_path=None):
         """
         Copy local file to Dropbox
 
@@ -114,7 +114,14 @@ class Dropbox(cmd.Cmd):
         """
         # Sanitization checking
         if os.path.isfile(from_path):
-            print "File exist"
+            self.do_encrypt_file(KEY, from_path)
+            out_filename = from_path + '.enc'
+
+            if not to_path:
+                to_path = '~/'
+
+            from_file = open(os.path.expanduser(out_filename), "rb")
+            self.api_client.put_file(self.current_path + "/" + to_path, from_file)
 
         else:
             print "Give a valid input file"
@@ -126,6 +133,74 @@ class Dropbox(cmd.Cmd):
         for line in f:
             sys.stdout.write(line)
 
+    def do_encrypt_file(self, key, in_filename, out_filename=None, chunksize=64*1024):
+        """ Encrypts a file using AES (CBC mode) with the
+            given key.
+
+            key:
+                The encryption key - a string that must be
+                either 16, 24 or 32 bytes long. Longer keys
+                are more secure.
+
+            in_filename:
+                Name of the input file
+
+            out_filename:
+                If None, '<in_filename>.enc' will be used.
+
+            chunksize:
+                Sets the size of the chunk which the function
+                uses to read and encrypt the file. Larger chunk
+                sizes can be faster for some files and machines.
+                chunksize must be divisible by 16.
+        """
+        if not out_filename:
+            out_filename = in_filename + '.enc'
+
+        iv = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
+        encryptor = AES.new(key, AES.MODE_CBC, iv)
+        filesize = os.path.getsize(in_filename)
+
+        with open(in_filename, 'rb') as infile:
+            with open(out_filename, 'wb') as outfile:
+                outfile.write(struct.pack('<Q', filesize))
+                outfile.write(iv)
+
+                while True:
+                    chunk = infile.read(chunksize)
+                    if len(chunk) == 0:
+                        break
+                    elif len(chunk) % 16 != 0:
+                        chunk += ' ' * (16 - len(chunk) % 16)
+
+                    outfile.write(encryptor.encrypt(chunk))
+              
+
+    def do_decrypt_file(self, key, in_filename, out_filename=None, chunksize=24*1024):
+        """ Decrypts a file using AES (CBC mode) with the
+            given key. Parameters are similar to encrypt_file,
+            with one difference: out_filename, if not supplied
+            will be in_filename without its last extension
+            (i.e. if in_filename is 'aaa.zip.enc' then
+            out_filename will be 'aaa.zip')
+        """
+        if not out_filename:
+            out_filename = os.path.splitext(in_filename)[0]
+
+        with open(in_filename, 'rb') as infile:
+            origsize = struct.unpack('<Q', infile.read(struct.calcsize('Q')))[0]
+            iv = infile.read(16)
+            decryptor = AES.new(key, AES.MODE_CBC, iv)
+
+            with open(out_filename, 'wb') as outfile:
+                while True:
+                    chunk = infile.read(chunksize)
+                    if len(chunk) == 0:
+                        break
+                    outfile.write(decryptor.decrypt(chunk))
+
+                outfile.truncate(origsize)
+            
 def main():
     drop = Dropbox()
 
